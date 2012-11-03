eb050a7c3a6388ee71d9123e47a2306bf8c9c19a
819419423 <daijing24@gmail.com>
2012-11-03 15:56:17 -0500

    Initial Commit

---
+++ MazeGame/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MazeGame
+//
+//  Created by Kobe Dai on 11/3/12.
+//  Copyright (c) 2012 Kobe Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MazeGame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MazeGame
+//
+//  Created by Kobe Dai on 11/3/12.
+//  Copyright (c) 2012 Kobe Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
